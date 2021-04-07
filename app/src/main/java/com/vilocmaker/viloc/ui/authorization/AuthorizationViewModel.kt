b17c7de6549914134d10f00e2a93f009d0709bc6
farrelfatah <farrelfma@gmail.com>
2021-04-07 16:10:46 +0700

    4th commit

--- app/src/main/java/com/vilocmaker/viloc/ui/authorization/AuthorizationViewModel.kt
+++ app/src/main/java/com/vilocmaker/viloc/ui/authorization/AuthorizationViewModel.kt
@@ -42,6 +42,6 @@ class AuthorizationViewModel(private val authorizationRepository: AuthorizationR
 
     // A placeholder password validation check
     private fun isPasswordValid(password: String): Boolean {
-        return password.length > 8
+        return password.length > 7
     }
 }
